617b03b079062e48bcc3ce368fc875e5565ed91b
Daniel Radnor <dannyradnor@hotmail.com>
2018-02-13 05:45:04 +0000

    Added date helper and cashflow table to scenario#show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  
+  def date_array(start_date, term_in_years)
+    y=term_in_years
+    array = Array.new(y)
+    date = start_date
+    array.each_with_index do |val, i|
+      if i == 0
+        array[i] = start_date
+      else
+        array[i] = date
+      end
+      date += 1.year
+    end
+  end
+  
 end
